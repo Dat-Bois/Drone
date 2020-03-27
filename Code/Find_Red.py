1430faed8b45cf9becc101c61a14b98c32f8c22b
unknown <eeshvij@gmail.com>
2020-03-26 20:21:03 -0700

    add text on frame

--- Code/Find_Red.py
+++ Code/Find_Red.py
@@ -19,18 +19,22 @@ def get_red(frame):
     kernel = np.ones((3,3),np.uint8)
     eroded = cv2.erode(mask_final, kernel, iterations=0)
     #dilated = cv2.dilate(mask_final, kernel, iterations=3)
+    font = cv2.FONT_HERSHEY_SIMPLEX
     cnts = cv2.findContours(eroded.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-
+    values = 0
     if len(cnts) > 0:
         c = max(cnts, key=cv2.contourArea)
         area = cv2.contourArea(c)
-        print(area)
+        #print(area)
         if(area > 20):
             ((x, y), radius) = cv2.minEnclosingCircle(c)
             x2, y2, w, h = cv2.boundingRect(c)
             cv2.rectangle(frame, (x2, y2), (x2+w, y2+h), (0, 255, 0), 2)
             cv2.circle(frame, (int(x), int(y)), int(radius), (255, 255, 255), 5, 2)
-            values = [(x2, y2), (x2+w,y2), (x2,y2+h), (x2+w,y2+h), (x,y), radius]
-            print(values)
-    result = cv2.bitwise_and(frame, frame, mask=eroded)
-    cv2.imshow("Result", result)
+            values = [(x2, y2), (x2+w,y2), (x2,y2+h), (x2+w,y2+h), (int(x),int(y)), int(radius)]
+            #print(values)
+        else:
+            values = [(0, 0), (0, 0), (0, 0), (0, 0), (0, 0), 0]
+    cv2.putText(frame,str(values),(10,30), font, 0.5,(255,255,255),2,cv2.LINE_AA)
+    cv2.imshow("Result", frame)
+    return values
